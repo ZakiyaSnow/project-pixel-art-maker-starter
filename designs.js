166e4bd036573fe918ce6a3f17920b8f7b9573e6
Zakiya Moore-Snowden <zakiya.moore226@gmail.com>
2022-04-26 00:36:42 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,36 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(){
+    const rows = document.getElementById('inputHeight').value;
+    const cols = document.getElementById('inputWidth').value;
 
-// Your code goes here!
+    // Select element to create grid
+    const tbl = document.getElementById('pixelCanvas');
 
+    // Clear table before creating grid
+    tbl.innerHTML="";
+
+    for (i = 0; i < rows; i++) {
+        let myRow =document.createElement('tr');
+        myRow = tbl.insertRow(i);
+    for (j = 0; j < cols; j++) {
+
+        let myCell = document.createElement('td');
+        myCell = myRow.insertCell(j);
+
+    // Add color to cell
+            myCell.addEventListener('click', () => {
+                myCell.style.backgroundColor = document.getElementById('colorPicker').value;
+            });
+        }
+    }
 }
+
+// Create form for submission
+const form = document.getElementById('sizePicker');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    makeGrid();
+});
